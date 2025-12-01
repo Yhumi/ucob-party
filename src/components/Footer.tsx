627ec0401cdcf8d50849ee2572880808e1636ee2
Yhumi <yhumi@miyei.me>
2025-12-01 20:09:00 +0000

    Add footer.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const FooterComponent = () => {
+  return (
+    <div className="footer-panel-container">
+      <span className="footer-text">Created by Yhumi Miyei @ Midgardsormr | Shows NA PF only (Sorry EU).</span>
+    </div>
+  )
+}
+
+export default FooterComponent;
