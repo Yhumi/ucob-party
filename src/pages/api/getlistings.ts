f625c0f047cb477f0f590b0f1a154e995dbc7481
Yhumi <yhumi@miyei.me>
2026-04-21 16:55:54 +0100

    Add test listings option to environment.

--- src/pages/api/getlistings.ts
+++ src/pages/api/getlistings.ts
@@ -1,17 +1,30 @@
 import type { APIRoute } from "astro";
+import testData from "../../../data/pf-test.json";
+
+const SHOW_TEST_LISTINGS = (import.meta.env.SHOW_TEST_LISTINGS ?? "false") === 'true';
 
 const getHandler = async () => {
-  const response = await fetch("http://api.miyei.me:7050/listings/ucob", {
-    method: 'GET',
-  });
+  if (SHOW_TEST_LISTINGS) {
+    return new Response(
+      JSON.stringify({
+        success: true,
+        data: testData
+      }),
+      { status: 200 }
+    );
+  } else {
+    const response = await fetch("http://api.miyei.me:7050/listings/ucob", {
+      method: 'GET',
+    });
 
-  return new Response(
-    JSON.stringify({
-      success: true,
-      data: await response.json()
-    }),
-    { status: 200 }
-  );
+    return new Response(
+      JSON.stringify({
+        success: true,
+        data: await response.json()
+      }),
+      { status: 200 }
+    );
+  }
 }
 
 //export const GET: APIRoute = rateLimiter(1, 20 * 1000)(getHandler);
